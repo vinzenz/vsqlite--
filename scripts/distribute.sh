f47e88497d7cc83ebf6848c3d9a3d12f397c3e7a
Vinzenz Feenstra <evilissimo@gmail.com>
2012-09-24 15:40:23 +0200

    Changing distribute

--- scripts/distribute.sh
+++ scripts/distribute.sh
@@ -1,18 +1,4 @@
 #!/bin/sh
 
-echo "Creating autotools environment"
-autoreconf --install --force --verbose
-echo "Running configure..."
-./configure --prefix=/usr > /dev/null
-echo "Done."
-echo "Making distribution packages..."
-make dist-clean
-make dist-gzip 
-make dist-bzip2 
-make dist-xz 
-make dist-zip
-echo "Moving files one level below"
-mv vsqlite++*.tar.* ..
-mv vsqlite++*.zip ..
-echo "Running cleanup"
 git clean -d -f
+
