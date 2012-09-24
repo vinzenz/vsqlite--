f62b788ec8d948c05c948ddfb090f18f9914b6e4
Vinzenz Feenstra <evilissimo@gmail.com>
2012-09-24 15:53:17 +0200

    Updated distribute

--- scripts/distribute.sh
+++ scripts/distribute.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
+DIST_VERSION=`cat VERSION`
+DIST_NAME='vsqlite++'
+DIST_FULL=$DIST_NAME-$DIST_VERSION
+echo "Generating $DIST_FULL..."
+
 git clean -d -f
+git archive tags/$DIST_VERSION --format=tar --prefix="$DIST_FULL/" | bzip2 > $DIST_FULL.tar.bz2
+git archive tags/$DIST_VERSION --format=tar --prefix="$DIST_FULL/" | gzip > $DIST_FULL.tar.gz
+git archive tags/$DIST_VERSION --format=tar --prefix="$DIST_FULL/" | xz -c > $DIST_FULL.tar.xz
+git archive tags/$DIST_VERSION --format=zip --prefix="$DIST_FULL/" > $DIST_FULL.zip
 
