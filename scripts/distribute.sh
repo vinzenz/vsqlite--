857c14e2151f9d11b616bc1ad336a7d13b3da702
Vinzenz Feenstra <evilissimo@gmail.com>
2012-09-24 15:25:10 +0200

    Adding distribute script

---
+++ scripts/distribute.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "Creating autotools environment"
+autoreconf --install --force --verbose
+echo "Running configure..."
+./configure --prefix=/usr > /dev/null
+echo "Done."
+echo "Making distribution packages..."
+make dist-gzip dist-bzip2 dist-xz dist-zip
+echo "Moving files one level below"
+mv *.tar.* ..
+echo "Running cleanup"
+git clean -d -f
