dab93f24523708f882c496967af3fa7d6461a878
Vinzenz Feenstra <evilissimo@gmail.com>
2012-09-25 12:58:39 +0200

    Updated distribute

--- scripts/distribute.sh
+++ scripts/distribute.sh
@@ -6,8 +6,14 @@ DIST_FULL=$DIST_NAME-$DIST_VERSION
 echo "Generating $DIST_FULL..."
 
 git clean -d -f
-git archive tags/$DIST_VERSION --format=tar --prefix="$DIST_FULL/" | bzip2 > $DIST_FULL.tar.bz2
-git archive tags/$DIST_VERSION --format=tar --prefix="$DIST_FULL/" | gzip > $DIST_FULL.tar.gz
-git archive tags/$DIST_VERSION --format=tar --prefix="$DIST_FULL/" | xz -c > $DIST_FULL.tar.xz
-git archive tags/$DIST_VERSION --format=zip --prefix="$DIST_FULL/" > $DIST_FULL.zip
+#git archive tags/$DIST_VERSION --format=tar --prefix="$DIST_FULL/" | bzip2 > $DIST_FULL.tar.bz2
+#git archive tags/$DIST_VERSION --format=tar --prefix="$DIST_FULL/" | gzip > $DIST_FULL.tar.gz
+#git archive tags/$DIST_VERSION --format=tar --prefix="$DIST_FULL/" | xz -c > $DIST_FULL.tar.xz
+#git archive tags/$DIST_VERSION --format=zip --prefix="$DIST_FULL/" > $DIST_FULL.zip
+./autogen.sh
+./configure --prefix=/usr
+make dist-gzip
+make dist-bzip2
+make dist-xz
+make dist-zip
 
